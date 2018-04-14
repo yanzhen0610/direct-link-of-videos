54f04830eb1c319fa067e02092aa7841059403d9
yanzhen0610 <yanzhen610@gmail.com>
2018-04-14 17:21:41 +0800

    fix some bugs

--- youtube.php
+++ youtube.php
@@ -3,6 +3,8 @@ header( 'Access-Control-Allow-Origin: *' );
 header( 'Content-Type: application/json' );
 
 $id = "";
+$title = "";
+$title_encoded = "";
 $result = array();
 $error = false;
 $error_msg = "";
@@ -16,8 +18,6 @@ if ( isset($_REQUEST['url']) && $_REQUEST['url'] != "" )
   // desktop site
   if ( preg_match("/(^https?\:\/\/)?(\w+\.)?youtube\.com\/watch\?(.*&)?v=([A-Za-z0-9_-]+)/", $url, $r) )
   {
-    // parse_str( parse_url( $_GET['url'], PHP_URL_QUERY ), $vars );
-    // $id = $vars['v'];
     $id = $r[4];
   }
   // mobile
@@ -36,7 +36,7 @@ else if ( isset($_REQUEST['v']) && $_REQUEST['v'] != "" )
 if ($id != "")
 {
   // load info from 'https://www.youtube.com/get_video_info'
-  $info = file_get_contents("http://www.youtube.com/get_video_info?video_id=$id&el=embedded&ps=default&eurl=&gl=US&hl=en");
+  $info = file_get_contents("http://www.youtube.com/get_video_info?eurl=http%3A%2F%2Fexample.com%2F&sts=17632&video_id=$id");
 
   // if load info successfully
   if ($info)
@@ -77,39 +77,15 @@ else
 // if there are no errors
 if (!$error)
 {
+  // get the title fo the video
   $title = $video_info['title'];
   $title_encoded = urlencode($title);
 
-  $streams = explode(',', $video_info['url_encoded_fmt_stream_map']);
-  $url_encoded_fmt_stream_map = array();
-  foreach ($streams as $stream)
-  {
-    $rows = explode('&', $stream);
-    $x = array();
-    foreach ($rows as $row)
-    {
-      $col = explode('=', $row);
-      $key = $col[0]; $value = urldecode($col[1]);
-      $x[$key] = $value;
-    }
-    $x['download_url'] = $x['url']."&title=".$title_encoded;
-    array_push($url_encoded_fmt_stream_map, $x);
-  }
+  $fmts = explode(',', $video_info['url_encoded_fmt_stream_map']);
+  $url_encoded_fmt_stream_map = parse_yt_fmts($fmts);
 
-  $streams = explode(',', $video_info['adaptive_fmts']);
-  $adaptive_fmts = array();
-  foreach ($streams as $stream)
-  {
-    $rows = explode('&', $stream);
-    $x = array();
-    foreach ($rows as $row)
-    {
-      $col = explode('=', $row);
-      $key = $col[0]; $value = urldecode($col[1]);
-      $x[$key] = $value;
-    }
-    array_push($adaptive_fmts, $x);
-  }
+  $fmts = explode(',', $video_info['adaptive_fmts']);
+  $adaptive_fmts = parse_yt_fmts($fmts);
 
   $result['status'] = "ok";
   $result['id'] = $id;
@@ -126,3 +102,63 @@ else
 
 // response the result as JSON
 echo json_encode($result, JSON_PRETTY_PRINT);
+
+///////////////
+// functions //
+///////////////
+
+function parse_yt_fmts($str_arr)
+{
+  $fmts = array();
+  foreach($str_arr as $str)
+  {
+    $rows = explode('&', $str);
+    $x = array();
+    foreach($rows as $row)
+    {
+      $col = explode('=', $row);
+      $key = $col[0]; $value = urldecode($col[1]);
+      $x[$key] = $value;
+    }
+    if (array_key_exists('url', $x))
+    {
+      // add the signature
+      if (array_key_exists('sig', $x))
+        $x['url'] .= "&signature=$x[sig]";
+      else if (array_key_exists('signature', $x))
+        $x['url'] .= "&signature=$x[signature]";
+      else if (array_key_exists('s', $x))
+        $x['url'] .= "&signature=" . sig($x['s']);
+
+      // add the encoded title to the url
+      global $title_encoded;
+      $x['dl_url'] = $x['url'] . "&title=$title_encoded";
+    }
+    array_push($fmts, $x);
+  }
+  return $fmts;
+}
+
+function str_swap(&$str, $i, $j)
+{
+  $tmp = $str[$i];
+  $str[$i] = $str[$j];
+  $str[$j] = $tmp;
+}
+
+function sig($sig)
+{
+  $exchanges = array(
+    array(1, 71),
+    array(1, 16),
+    array(1, 4)
+  );
+  $slice = array(3, 2);
+  foreach($exchanges as $ex)
+  {
+    str_swap($sig, $ex[0], $ex[1]);
+  }
+  $sig = substr($sig, $slice[0], strlen($sig) - $slice[0] - $slice[1]);
+  $sig = strrev($sig);
+  return $sig;
+}
